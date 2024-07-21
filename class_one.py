6ec6dc6899f97cc4163d758a162e17c342d6d76f
alihamza12-ai <ah6825169@gmail.com>
2024-07-21 06:25:43 -0700

    dict(comprehensive style)

--- class_one.py
+++ class_one.py
@@ -1,6 +1,8 @@
-#comprehensive style for Dict
-import pprint
+
 from typing import Dict
+#it is a built in function work like a consol function
+print("Hillo world")
+#comprehensive style for Dict
 data : Dict[str,str] ={"Name":"ali hamza",
-                      "fname":"zakaullah",}
-{v:k for k,v in data.items()}
+                      "fname":"zakaullah"}
+{v :k for k,v in data.items()}
